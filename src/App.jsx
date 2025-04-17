775884b37ed0b9f049d60f12befa06f4ee26f3bf
ALENUI <rovisalenui@gmail.com>
2025-04-17 13:20:10 +0100

    darktheme + toggle dark + deploment on free route + display network route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -7,10 +7,12 @@ import Profile from "./pages/Profile";
 import GroupPage from "./pages/GroupPage";
 
 import "./index.css";
+import Navbar from "./components/themeTogglt-Btn/navbar/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
